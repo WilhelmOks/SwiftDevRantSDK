6a08cf8b4812d218069caed430d71210b8c590b4
Wilhelm Oks <oksw@Mac.fritz.box>
2024-12-25 18:39:03 +0100

    KreeRequest 2.0.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/WilhelmOks/KreeRequest", .upToNextMajor(from: "1.0.3")),
+        .package(url: "https://github.com/WilhelmOks/KreeRequest", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
